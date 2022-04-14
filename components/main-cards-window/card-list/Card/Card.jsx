7d506249a509607c8adeb5b323e866e5a0631ec4
Turan141 <darkhero-95@mail.ru>
2022-04-15 00:17:39 +0400

    a lot of changes

--- components/main-cards-window/card-list/Card/Card.jsx
+++ components/main-cards-window/card-list/Card/Card.jsx
@@ -3,22 +3,43 @@ import Image from "next/image"
 import style from "./Card.module.scss"
 
 export const Card = ({ currentItems }) => {
+	console.log(currentItems)
 	const map = currentItems ? (
 		currentItems.map((item) => (
-			<div className={style.person__card}>
+			<div key={item.id} className={style.person__card}>
 				<div className={style.img}>
 					<Image
 						className={style.blur}
-						src={item.images.sm}
-						// width='160px'
-						// height='240px'
-						height='120px'
-						width='160px'
+						src={item.images.xs}
+						height='150px'
+						width='225px'
 					/>
 				</div>
-				<div className='style person_info'>
+				<div className={style.rounded_avatar}>
+					<Image
+						className={style.char_avatar}
+						src={item.images.sm}
+						width='150px'
+						height='150px'
+					/>
+				</div>
+				<div className={style.person_info}>
 					<h1>{item.name}</h1>
-					<h2>{item.race}</h2>
+					<h2>{item.appearance.race ? item.appearance.race : "Unknown"}</h2>
+					<div className={style.stat_div}>
+						<ul className={style.card_stats}>
+							<div className={style.left_stat}>
+								<li>Combat: {item.powerstats.combat}</li>
+								<li>Durability: {item.powerstats.durability}</li>
+								<li>Intelligence: {item.powerstats.intelligence}</li>
+							</div>
+							<div className={style.right_stat}>
+								<li>Speed: {item.powerstats.speed}</li>
+								<li>Power: {item.powerstats.power}</li>
+								<li>Strength: {item.powerstats.strength}</li>
+							</div>
+						</ul>
+					</div>
 				</div>
 			</div>
 		))
