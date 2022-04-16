18021e6966097a4d4bd11b16c3ea534169511575
Turan <90398677+Turan141@users.noreply.github.com>
2022-04-16 22:50:14 +0400

    . .

--- components/main-cards-window/card-list/paginate/PaginatedItems.jsx
+++ components/main-cards-window/card-list/paginate/PaginatedItems.jsx
@@ -1,48 +1,43 @@
-import { useEffect, useState } from "react"
+import {useEffect, useState} from "react"
 import ReactPaginate from "react-paginate"
-import { Card } from '../card/Card'
-import { useGetCharactersQuery } from "../../../../redux-state/query"
+import {Card} from "../card/Card"
+
+import {useGetCharactersQuery} from "../../../../redux-state/query"
 
 import style from "./Paginate.module.scss"
 
-export const PaginatedItems = ({ itemsPerPage }) => {
-	const [currentItems, setCurrentItems] = useState(null)
-	const [pageCount, setPageCount] = useState(0)
-	const [itemOffset, setItemOffset] = useState(0)
+export const PaginatedItems = ({itemsPerPage}) => {
+  const [currentItems, setCurrentItems] = useState(null)
+  const [pageCount, setPageCount] = useState(0)
+  const [itemOffset, setItemOffset] = useState(0)
 
-	const { data, isLoading, error } =
-		useGetCharactersQuery("all")
+  const {data, isLoading, error} = useGetCharactersQuery("all")
 
-	useEffect(() => {
-		const endOffset = itemOffset + itemsPerPage
-		data && setCurrentItems(data.slice(itemOffset, endOffset))
-		data && setPageCount(Math.ceil(data.length / itemsPerPage))
-	}, [itemOffset, itemsPerPage, data])
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage
+    data && setCurrentItems(data.slice(itemOffset, endOffset))
+    data && setPageCount(Math.ceil(data.length / itemsPerPage))
+  }, [itemOffset, itemsPerPage, data])
 
-	const handlePageClick = (event) => {
-		const newOffset =
-			(event.selected * itemsPerPage) % data.length
-		setItemOffset(newOffset)
-	}
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length
+    setItemOffset(newOffset)
+  }
 
-	return (
-		<div className={style.card_paginate_combo}>
-			<Card
-				currentItems={currentItems}
-				isLoading={isLoading}
-				error={error}
-			/>
-			<div className={style.paginate}>
-				<ReactPaginate
-					breakLabel='...'
-					nextLabel='next'
-					onPageChange={handlePageClick}
-					pageRangeDisplayed={3}
-					pageCount={pageCount}
-					previousLabel='prev'
-					renderOnZeroPageCount={null}
-				/>
-			</div>
-		</div>
-	)
+  return (
+    <div className={style.card_paginate_combo}>
+      <Card currentItems={currentItems} isLoading={isLoading} error={error} />
+      <div className={style.paginate}>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          pageCount={pageCount}
+          previousLabel="prev"
+          renderOnZeroPageCount={null}
+        />
+      </div>
+    </div>
+  )
 }
